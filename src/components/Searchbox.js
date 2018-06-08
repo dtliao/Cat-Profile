958cf78a137150e033e69722e9d34e40136a1ee7
Daniel Liao <39768672+dtliao@users.noreply.github.com>
2018-06-08 23:08:46 +0800

    initial release

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -4,7 +4,7 @@ const Searchbox = ({ searchfield,SearchChange }) => {
 	return(
 		<div className='pa2'>
 			<input 
-				className='pa3 ba w-20 fw5 f3 helvetica'
+				className='pa3 ba w-10 fw5 f3 helvetica'
 				type='search' 
 				placeholder= 'search cats'
 				onChange={SearchChange}
